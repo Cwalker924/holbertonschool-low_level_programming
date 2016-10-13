f11da21a574e19e661bf117b513a770fc7f7904e
Cwalker924 <cwalker924@gmail.com>
2016-10-13 01:51:31 +0000

    fix for 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,7 +8,7 @@
  * prints using a string using puts
  * Return: 0 to close main
  */
-int main(0)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,")
 	return (0);
