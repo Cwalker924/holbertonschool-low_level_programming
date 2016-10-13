a35d25c5cd47601ca3ffc3e5ddc21c363f2feb48
Cwalker924 <cwalker924@gmail.com>
2016-10-13 01:31:54 +0000

    Adding 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,17 @@
-#!/bin/bash
 #include <stdio.h>
+/*
+ * C program
+ */
 
+
+/**
+ * main - for entry point
+ * prints using a string using puts
+ * Return: 0 to close main 
+ *
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
