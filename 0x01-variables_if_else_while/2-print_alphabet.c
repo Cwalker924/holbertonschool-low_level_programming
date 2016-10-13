05243dba4f3dd94e603df3a6326550413dfef3ef
Cwalker924 <cwalker924@gmail.com>
2016-10-13 22:22:26 +0000

    adding 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
