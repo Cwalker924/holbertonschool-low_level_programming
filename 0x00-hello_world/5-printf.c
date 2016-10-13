6cceca13ea76c96ba3beb23da8e51279f9206ed5
Cwalker924 <cwalker924@gmail.com>
2016-10-13 01:50:17 +0000

    Adding 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * C program that prints a string
+ */
+
+/**
+ * main - for entry point
+ * prints using a string using puts
+ * Return: 0 to close main
+ */
+int main(0)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+	return (0);
+}
