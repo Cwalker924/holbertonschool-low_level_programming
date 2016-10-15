2ef5ef116cbb608cd7182e2ef5a2fe4df36eea3a
Cwalker924 <cwalker924@gmail.com>
2016-10-15 01:23:15 +0000

    adding 11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Returns: 0
+ */
+int main(void)
+{
+	int n, num;
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+		for (num = '1' ; num <= '9' ; num++)
+		{
+			if (n < num)
+			{
+				putchar(n);
+				putchar(num);
+				if (n == '8' && num == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
