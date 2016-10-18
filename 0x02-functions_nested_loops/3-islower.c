4c4e41025a0bcb34c0bf374c8f7c348554873d18
Cwalker924 <cwalker924@gmail.com>
2016-10-18 22:06:25 +0000

    Changes to letter 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _islower - returns if input is lowercase alph.
+ * @c: checks the case
+ * Return: 0 or 1
+ */
 int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
