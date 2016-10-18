2cea2814b100fef8d2abd70f7684094ca0164304
Cwalker924 <cwalker924@gmail.com>
2016-10-18 21:34:28 +0000

    commit for header

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+
 void print_alphabet(void);
 int _islower(int c);
 int _isalpha(int c);
@@ -8,5 +11,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);
-void _putchar(int);
+void print_to_98(int);
+void _putchar(char c);
+
+#endif
