ee872895fdd03da89022ea135a9d515eff44df2d
Cwalker924 <cwalker924@gmail.com>
2016-10-18 19:43:24 +0000

    Made directory name change

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * jack_bauer - returns every minute of the day for JB
+ */
+void jack_bauer(void)
+{
+	int hrs, min;
+
+	hrs = 0;
+	while (hrs < 24)
+	{
+		min = 0;
+		while (min < 60)
+		{
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+			min++;
+		}
+		hrs++;
+	}
+}
