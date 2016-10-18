6d06d270a8431235a65ad340d667a52cf9f1e8dd
Cwalker924 <cwalker924@gmail.com>
2016-10-18 22:19:43 +0000

    fix for 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 /**
- *
- *
+ * _abs - returns the absolute value of arg
+ * @c: check case
+ * Return: absolute value
  */
 int _abs(int c)
 {
 	if (c < 0)
 	{
-		return(c * -1);
+		return (c * -1);
 	}
 	else
 	{
