2b14629ab68f67f26b357ec40453e97e92bbf95e
Cwalker924 <cwalker924@gmail.com>
2016-10-19 06:36:49 +0000

    adding 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints from a given number to 98
+ * @n: check case
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	printf("\n");
+}
