b5d532d65c66472cc63aa3ffeb120f25fdd1e790
Cwalker924 <cwalker924@gmail.com>
2016-10-20 20:39:27 +0000

    adding 3 & 4

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_numbers - print 0-9
+ */
+void print_numbers(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		_putchar(n + '0');
+		n++;
+	}
+	_putchar('\n');
+}
