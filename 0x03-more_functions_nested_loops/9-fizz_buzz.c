cc8416e29c211017f1c649341d52149031329148
Cwalker924 <cwalker924@gmail.com>
2016-10-21 01:20:50 +0000

    adding 9

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,11 @@ int main(void)
 	n = 1;
 	while (n <= 100)
 	{
-		if (n % 3 == 0 && n % 5 == 0)
+		if (n == 100)
+		{
+			printf("Buzz");
+		}
+		else if (n % 3 == 0 && n % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
