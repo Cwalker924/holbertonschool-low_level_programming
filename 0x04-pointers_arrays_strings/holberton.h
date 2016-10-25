899702207a6a8629c531495ae891f8455ce9cd60
Cwalker924 <cwalker924@gmail.com>
2016-10-25 12:51:21 -0700

    Update holberton.h

--- 0x04-pointers_arrays_strings/holberton.h
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -9,3 +12,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 void _putchar(char c);
+
+#endif
