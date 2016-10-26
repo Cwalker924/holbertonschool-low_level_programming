be40daaeaae79efc9bfc643aa613b30ec515ae33
Cwalker924 <cwalker924@gmail.com>
2016-10-26 02:32:23 +0000

    adding 9

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,23 @@
 #include "holberton.h"
 
 /**
- * _strcpy
- *
- *
+ * _strcpy - redirects memory
+ * @dest: destination
+ * @src: source
+ * Retrun: new destination
  */
-char *_strcpy(char *dest, char *src){}
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	char *get;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	get = dest;
+	return(get);
+}
