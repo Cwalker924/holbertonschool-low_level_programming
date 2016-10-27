b5e54f0afd4aadff076a6fea91b229b88049e754
Cwalker924 <cwalker924@gmail.com>
2016-10-27 23:51:46 +0000

    adding 3

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -8,30 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
-
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		i++;
-	}
+	int j;
 
 	j = 0;
-	while (s2[j] != '\0')
+	while (s1[j] != '\0' && s2[j] != '\0')
 	{
+		if (s1[j] != s2[j])
+			return ((s1[j] - '0') - (s2[j] - '0'));
 		j++;
 	}
-
-	if (i < j)
-	{
-		return (-15);
-	}
-	else if (i > j)
-	{
-		return (15);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
