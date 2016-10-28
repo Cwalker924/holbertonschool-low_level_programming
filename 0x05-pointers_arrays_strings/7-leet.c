e3e936b938f260ea469a545fa3c52d30899c9e7d
Cwalker924 <cwalker924@gmail.com>
2016-10-28 03:01:14 +0000

    adding 7

---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * leet - translates specific characters
+ * @p: pointer variable
+ */
+char *leet(char *p)
+{
+	int i = 0;
+
+	while (p[i] != '\0')
+	{
+		while (p[i] == 'a' || p[i] == 'A')
+		{
+			p[i] = '4';
+		}
+		while (p[i] == 'e' || p[i] == 'E')
+		{
+			p[i] = '3';
+		}
+		while (p[i] == 'o' || p[i] == 'O')
+		{
+			p[i] = '0';
+		}
+		while (p[i] == 't' || p[i] == 'T')
+		{
+			p[i] = '7';
+		}
+		while (p[i] == 'l' || p[i] == 'L')
+		{
+
+			p[i] = '1';
+		}
+		i++;
+	}
+	return (p);
+}
