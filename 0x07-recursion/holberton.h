25f03be301b9d6493f60e188197317106ae4d243
Cwalker924 <cwalker924@gmail.com>
2016-11-02 19:32:26 +0000

    adding dr and files

---
+++ 0x07-recursion/holberton.h
@@ -0,0 +1,9 @@
+int _putchar(char c);
+ _puts_recursion(char *s);
+ _print_rev_recursion(char *s);
+_strlen_recursion(char *s);
+factorial(int n);
+_pow_recursion(int x, int y);
+_sqrt_recursion(int n);
+is_prime_number(int n);
+is_palindrome(char *s);
