307c551d25b68d8f542d1dcae974e0e4cd6adf47
Cwalker924 <cwalker924@gmail.com>
2016-11-11 21:52:54 +0000

    fix for 1

--- 0x0B-more_malloc_free/1-main.c
+++ 0x0B-more_malloc_free/1-main.c
@@ -9,13 +9,9 @@
  */
 int main(void)
 {
-	char *concat, *test1, *test2;
+	char *concat;
 
 	concat = string_nconcat("Holberton ", "School !!!", 6);
-	test1 = string_nconcat("2345", "23452462457", 6);
-	test2 = string_nconcat("oh ", "hello", 7);
-	printf("%s\n", test1);
-	printf("%s\n", test2);
 	printf("%s\n", concat);
 	free(concat);
 	return (0);
--- 0x0B-more_malloc_free/1-string_nconcatTest.c
+++ 0x0B-more_malloc_free/1-string_nconcatTest.c
@@ -5,7 +5,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *c;
-	unsigned int i, a, b;
+	unsigned int a, b, i;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -19,19 +19,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	i = 0;
 	if (n >= b)
+	{
 		i = a + b;
+	}
 	else
+	{
 		i = a + n;
+	}
 
 	c = malloc((i + 1) * sizeof(*c));
 
 	if (c == NULL)
+	{
 		return (NULL);
+	}
 
 	for (a = 0; s1[a] != '\0'; a++)
+	{
 		c[a] = s1[a];
-	for (b = 0; a < i && s2[b] != '\0'; a++, b++)
+	}
+	for (b = 0; a < i && s2[b] != '\0'; b++, a++)
+	{
 		c[a] = s2[b];
+	}
 	c[a] = '\0';
 	return (c);
 }
