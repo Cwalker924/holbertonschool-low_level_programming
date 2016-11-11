89d58b446e94c0fcafac93faf196f0734de09cb3
Cwalker924 <cwalker924@gmail.com>
2016-11-11 21:57:12 +0000

    fix for 1

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n index of s2
+ * Return: string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *c;
