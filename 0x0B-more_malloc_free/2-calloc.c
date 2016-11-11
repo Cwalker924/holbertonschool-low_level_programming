a88eea3d31eb3e59d903bbe6eccac76c79a7fbed
Cwalker924 <cwalker924@gmail.com>
2016-11-11 00:43:08 +0000

    adding 2

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -3,4 +3,24 @@
 #include <stdlib.h>
 
 /**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: an array
+ * @size: size of array
+ * Return: void
  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *c;
+
+	if (size == 0 || nmemb == 0)
+	{
+		return (NULL);
+	}
+	c = malloc(nmemb);
+
+	if (c == NULL)
+	{
+		return (NULL);
+	}
+	return (c);
+}
