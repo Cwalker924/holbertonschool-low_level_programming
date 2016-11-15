37e03c00ca22b513ef9882817f622390be7c612d
Cwalker924 <cwalker924@gmail.com>
2016-11-15 06:43:15 +0000

    adding 1

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -2,12 +2,19 @@
 #include "dog.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * init_dog - initalize dog structure
+ * @d: dog structure pointer
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog's owner
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
