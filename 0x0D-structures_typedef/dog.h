706b851b1dba102ae42d2711d24c008b98ed7684
Cwalker924 <cwalker924@gmail.com>
2016-11-15 07:57:13 +0000

    fix for 2

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -9,12 +9,14 @@
  *
  * Description: defines a dog by name age and it's owner
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
