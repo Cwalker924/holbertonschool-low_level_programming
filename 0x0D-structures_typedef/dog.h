8cfec22564f4d4fd3a13ddb3a0fa03e1db10e108
Cwalker924 <cwalker924@gmail.com>
2016-11-15 06:15:56 +0000

    adding dir, 1

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct Dog - structure of a Dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog's owner
+ */
+struct Dog
+{
+	char *name;
+	float age;
+	char* owner;
+};
