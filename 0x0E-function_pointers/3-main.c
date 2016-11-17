48e66e92c67012979ee57fd2604ab893da083a7b
Cwalker924 <cwalker924@gmail.com>
2016-11-17 23:17:55 +0000

    fix for 3

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -15,13 +15,13 @@ int main(int argc, char *argv[])
 
 	if (argc != 4)
 	{
-		printf("ERROR");
+		printf("Error\n");
 		exit(98);
 	}
 
 	if (argv[2][1] != '\0')
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(99);
 	}
 
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 
 	if (o == NULL)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(99);
 	}
 
