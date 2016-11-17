4923656eaf6ee2814d20b2e9e73f89551d637b0d
Cwalker924 <cwalker924@gmail.com>
2016-11-17 22:53:08 +0000

    fixes for 3

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -17,15 +17,14 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
+	int i;
 
-	int i = 0;
-
+	i = 0;
 	while (i < 5)
 	{
 		if (s[0] == ops[i].op[0])
 		{
 			return (ops[i].f);
-			i++;
 		}
 		i++;
 	}
