54773797f931a029a46dfe935b5e44741ee9aa18
Cwalker924 <cwalker924@gmail.com>
2016-12-01 22:29:20 +0000

    fix 6

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -9,6 +9,8 @@ int pop_listint(listint_t **head)
 {
 	listint_t *pop;
 	int i;
+	if (head == NULL)
+		return (0);
 
 	pop = *head;
 	if (pop != NULL)
@@ -16,6 +18,7 @@ int pop_listint(listint_t **head)
 		*head = pop->next;
 		i = pop->n;
 		free(pop);
+		return (i);
 	}
 	return (0);
 }
