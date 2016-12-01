6665b05b2921eda7e7796b7d3c2feb6e6b3c3d39
Cwalker924 <cwalker924@gmail.com>
2016-12-01 20:29:52 +0000

    fix for 5

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -1,12 +1,15 @@
 #include "lists.h"
 
 /**
- * free_listint2 -
- * @head:
+ * free_listint2 - function that frees listsint_t
+ * @head: start of the linked list
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+
+	if (head == NULL)
+		return;
 
 	while (*head != NULL)
 	{
