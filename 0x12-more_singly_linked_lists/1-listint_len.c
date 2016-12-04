53aa2b79435b2b8c0c8b4c112d3ef7de18b9f781
Cwalker924 <cwalker924@gmail.com>
2016-12-04 01:55:09 +0000

    fix for 1 betty

--- 0x12-more_singly_linked_lists/1-listint_len.c
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * listsint_len - returns the number of elements in listsint_t
+ * listint_len - returns the number of elements in listsint_t
  * @h: start of linked list
  * Return: number of elements in list
  */
@@ -12,8 +12,8 @@ size_t listint_len(const listint_t *h)
 	i = 0;
 	while (h != NULL)
 	{
-		i++;
 		h = h->next;
+		i++;
 	}
 	return (i);
 }
