34ccf9a3c6c0118329bca114b0354fe4657970b4
Cwalker924 <cwalker924@gmail.com>
2016-12-04 02:22:46 +0000

    fix for 7

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -14,11 +14,15 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	i = 0;
-	while (i < index)
+	if (head->next != NULL)
 	{
-		head = head->next;
-		i++;
+		i = 0;
+		while (i < index)
+		{
+			head = head->next;
+			i++;
+		}
+		return (head);
 	}
-	return (head);
+	return (NULL);
 }
