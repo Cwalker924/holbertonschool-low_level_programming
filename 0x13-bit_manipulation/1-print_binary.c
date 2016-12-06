ef4a758d29a5f91e1814b06c02181cc56d8ed759
Cwalker924 <cwalker924@gmail.com>
2016-12-06 21:56:56 +0000

    adding 2

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == '1')
+		_putchar('1');
+	if (n == '0')
+		_putchar('0');
+	
+}
---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * get_bit - a function that returns the value of a bit at a
+ * given index.
+ * @n: number passed in.
+ * @index: place to evaluate
+ * Return: int 1 or 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int j, c;
+
+	j = n >> index;
+	c = j & 1;
+	if (c == 1)
+		return (1);
+	else
+		return (0);
+}
