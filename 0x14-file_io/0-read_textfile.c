3d3bac57b4699cdc48c45b5b9d6b7f5d5a0e2599
Cwalker924 <cwalker924@gmail.com>
2016-12-08 19:31:46 +0000

    fix for 0

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,26 +1,6 @@
 #include "holberton.h"
 
 /**
- * _print - prints string
- * @str: string to print
- * @max: letters to print
- * Return: number of characters
- */
-int _print(char *str, size_t max)
-{
-	int i;
-
-	i = 0;
-	while (str[i] != '\0' && max > 0)
-	{
-		_putchar(str[i]);
-		i++;
-		max--;
-	}
-	return (i);
-}
-
-/**
  * read_textfile - a function that reads a text file and
  * print it to the POSIX standard output.
  * @filename: name of file to be read
@@ -30,23 +10,33 @@ int _print(char *str, size_t max)
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char buf[1024];
-	ssize_t p;
+	char *buf;
+	ssize_t read_count, write_count;
 
 	if (!filename)
 		return (0);
 
 	/* opens Requiescat file */
-	fd = open(filename, O_RDONLY);
-
+	fd = open(filename, O_RDWR);
 	if (fd == -1)
 		return (0);
 	/* Reads Requiescat file */
-	read(fd, buf, letters);
-	buf[letters] = '\0';
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+	{
+		free(buf);
+		return (0);
+	}
+	read_count = read(fd, buf, letters);
+	if (read_count == -1)
+		return (0);
 
+	write_count = write(STDOUT_FILENO, buf, read_count);
+	if (write_count == -1)
+		return (0);
+
+        free(buf);
 	close(fd);
 
-	p = _print(buf, letters);
-	return (p);
+	return (write_count);
 }
