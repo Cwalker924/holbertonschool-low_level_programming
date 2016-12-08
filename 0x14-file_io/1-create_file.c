377e743195f09deed1cf96abc710e151fc129ad8
Cwalker924 <cwalker924@gmail.com>
2016-12-08 08:46:15 +0000

    adding 1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -24,15 +24,21 @@ int _strlen(char *s)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	
+	int fd, fd_write, length;
+	char *buf;
 
+	length = _strlen(text_content);
+	buf = malloc(sizeof(length));
+
+	/* checks if buf is NULL*/
+	if (!buf)
+		return (-1);
 	/* checks if file is NULL */
 	if (!filename)
 		return (-1);
 
 	/*creats and opens file with read/write permission*/
-	fd = open(filename, 0_RDWR | 0_creat, 0600);
+	fd = open(filename, O_CREAT | O_RDWR, 0600);
 	/* if file as an error */
 	if (fd == -1)
 		return (-1);
@@ -44,12 +50,12 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 	/* write text_content to fd*/
-	fd_write = write(fd, text_content, len);
-	if (fd == -1)
+	fd_write = write(fd, text_content, length);
+	if (fd_write == -1)
 	{
 		close(fd);
 		return (-1);
 	}
 	close(fd);
-	return (1):
+	return (1);
 }
