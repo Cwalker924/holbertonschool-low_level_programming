e309d940c92298876d3c6ad35a2e7782a99d845d
Cwalker924 <cwalker924@gmail.com>
2016-12-12 05:06:11 +0000

    fix for 1

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -12,15 +12,20 @@ void print_binary(unsigned long int n)
 	char checker, a;
 
 	checker = 0;
-	bit = sizeof(unsigned long int) * 8 -1;
+	bit = sizeof(unsigned long int) * 8 - 1;
 	if (n == 0)
 		_putchar('0');
 	while (bit >= 0)
 	{
 		hold = n >> bit;
 		a = hold & 1;
-		if (a == 1 && checker == 1)
+		if (a == 0 && checker == 1)
 			_putchar ('0');
+		else if (a == 1)
+		{
+			checker = 1;
+			_putchar ('1');
+		}
 		bit--;
 	}
 }
