5685ec7e5a3e4ef27415ad9231b8db6dde737884
Cwalker924 <cwalker924@gmail.com>
2016-12-12 05:07:41 +0000

    fix for 1

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * print_binary - a function that prints the binary representation of a number.
+ * @n: number to convert
  */
 void print_binary(unsigned long int n)
 {
