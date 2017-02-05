bab920edd5dd2d3b1796fee28f360b5b840df5bf
Cwalker924 <cwalker924@gmail.com>
2017-02-05 20:50:06 +0000

    fix for 1

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 
 /**
- *
- *
- *
+ * hash_djb2 - djb2 algorithm
+ * @str: pointer to a string
+ * Return: value created
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
