6856bd8f38c10345d3107bc49b247ef2d060e725
Cwalker924 <cwalker924@gmail.com>
2017-03-24 16:45:17 +0000

    fix for 17

--- 0x1C-binary_trees/17-binary_tree_sibling.c
+++ 0x1C-binary_trees/17-binary_tree_sibling.c
@@ -1,12 +1,24 @@
 #include "binary_trees.h"
 
 /**
- *
- *
- *
+ * binary_tree_sibling - a function that finds the sibling of a node
+ * @node: a pointer to the node to find the sibling of
+ * Return: sibling node value
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL)
+	binary_tree_t *npl, *npr;
+
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
+	npl = node->parent->left;
+	npr = node->parent->right;
+	if (npl != NULL && npr != NULL)
+	{
+		if (npr->n == node->n)
+			return (npl);
+		else
+			return (npr);
+	}
+	return (NULL);
 }
