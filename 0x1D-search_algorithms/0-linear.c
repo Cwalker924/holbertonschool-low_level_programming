21c04d65a5e7a4485864af24ce8db6eac304d93a
Cwalker924 <cwalker924@gmail.com>
2017-04-08 06:13:31 +0000

    adding format on 0

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -1,11 +1,12 @@
 #include "search_algos.h"
 
 /**
- * linear_search - 
- * @array: 
- * @size: 
- * @value: 
- * Return: 
+ * linear_search - a function that searches for a value in an array of
+ * integers using the Linear search algorithm
+ * @array: a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: the first index where value is located otherwise -1 if no match
  */
 int linear_search(int *array, size_t size, int value)
 {
