0c94c17e121d0c1c288745953fb28b69cc3a6b3b
Cwalker924 <cwalker924@gmail.com>
2017-04-08 19:27:55 +0000

    fix for 1

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -7,6 +7,7 @@
 #include <math.h>
 
 int linear_search(int *array, size_t size, int value);
+void print_array(int *array, size_t start, size_t end);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 
