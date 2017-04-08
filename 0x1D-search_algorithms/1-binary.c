73f491773ced58b78bdc010e0f84f501ce68c01d
Cwalker924 <cwalker924@gmail.com>
2017-04-08 20:12:38 +0000

    fix for 1

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -30,7 +30,7 @@ int binary_search(int *array, size_t size, int value)
 	size_t start, mid, end;
 	int *parray;
 
-	if (!array || size == 0)
+	if (!array || size <= 0)
 		return (-1);
 
 	parray = array;
