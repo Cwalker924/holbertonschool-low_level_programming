92149b0960f515d8bedf326a0700903bf6f2e59c
Cwalker924 <cwalker924@gmail.com>
2017-05-19 22:21:52 +0000

    adding 5

--- 0x1E-huffman_rb_trees/symbol.c
+++ 0x1E-huffman_rb_trees/symbol.c
@@ -1,20 +1,20 @@
 #include "huffman.h"
 
 /**
- * symbol_create - function that creates a new symbol data structure
- * @data: data that is to be stored in structure
- * @freq: frequienct that is to be stored in structure
+ * symbol_create - creates new symbol
+ * @data: data that is stored in structure
+ * @freq: frequiency that is to be stored in structure
  * Return: pointer to new structure
  */
 symbol_t *symbol_create(char data, size_t freq)
 {
-	symbol_t *new_symbol;
+	symbol_t *add_symbol;
 
-	new_symbol = malloc(sizeof(symbol_t));
-	if (!new_symbol)
-		return (NULL)
-	new_symbol->data = data;
-	new_symbol->freq = freq;
+	add_symbol = malloc(sizeof(symbol_t));
+	if (!add_symbol)
+		return (NULL);
+	add_symbol->data = data;
+	add_symbol->freq = freq;
 
-	return (new_symbol);
+	return (add_symbol);
 }
